a40ac1e80dd9c3ee0968612b6433d524de0eb09d
Justin <rmjustin02@gmail.com>
2020-06-05 12:13:05 +0200

    thema

---
+++ header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="<?php language_attributes() ?>">
+<head>
+    <meta charset="<?php bloginfo('charset') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Justin Montoya">
+    <title><?php bloginfo('name') ?></title>
+</head>
+
+<h1><?php bloginfo('name') ?></h1>
